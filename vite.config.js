5751fcd002c030db278ef2aec112c26be141d419
Matheus <silva.matheus6081@gmail.com>
2025-10-11 07:45:31 -0300

    Recipe Detail Page and Results Page

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        recipe_page: resolve(__dirname, "src/recipes-page/index.html"),
-        recipe_listing: resolve(__dirname, "src/recipes-listing/index.html"),
+        recipe_page: resolve(__dirname, "src/recipes_page/index.html"),
+        recipe_listing: resolve(__dirname, "src/recipes_listing/index.html"),
       },
     },
   },
